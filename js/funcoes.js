5cb7cb81bdf5f3b5ee0f8969835c7ce97f5d3fb6
Maria Eduarda <mariaeduarda.assuncaopontes26@gmail.com>
2022-12-09 19:53:24 -0300

    Atualizado

--- js/funcoes.js
+++ js/funcoes.js
@@ -8,15 +8,10 @@ x = xmlDoc.getElementsByTagName("postagem");
 function texto(){
         for(i = 0; i <= 1; i++){
             document.write( 
-                "<h1 class=>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</h1>" +
-                "<div class='texto'>");
-                for(i = 0; i < x.length; i++){
-                    document.write( 
-                        "<p>" + x[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue + "</p>");
-                }
-                document.write( 
-                "</div>"
-                );
+                "<div class='oque container'" +
+                    "<h1 class='inika'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</h1>" +
+                    "<p class=''>" + x[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue + "<p>" +
+                "</div>");
         }
     }
 
