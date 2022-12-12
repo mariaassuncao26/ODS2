e86a97b8dd3062e15fef98a3af1993036ef07390
Maria Eduarda <mariaeduarda.assuncaopontes26@gmail.com>
2022-12-12 20:35:30 -0300

    Atualizado

--- js/funcoes.js
+++ js/funcoes.js
@@ -47,11 +47,20 @@ function postagem(){
     //Armazena a id, ex: 2
     i = parametro.get("codigo_postagem");
 
-        document.write(
-            "<div>" +
-                "<div><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"></div>" +
-                "<div class='inika'>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</div>" +
-                "<div>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</div>" +
-            "</div>");
+    document.write(
+        "<div>" +
+                    "<div class='inika titulop'>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</div>" +
+            "<div class='container imagemp'>" +
+                "<div class='card'>" +
+                    "<div class=''><img class='card-img-top' style='width:100%' alt='Card image' src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='1000'></div>" +
+                "</div>" +
+            "</div>" +
+            "<div class='corpop'>" +
+                    "<i> <p class='comentario'>"+ x[i].getElementsByTagName("comentario")[0].childNodes[0].nodeValue + "</p> </i>" +
+                    "<p class=''>"+ x[i].getElementsByTagName("p1")[0].childNodes[0].nodeValue + "</p>" +
+                    "<p class=''>"+ x[i].getElementsByTagName("p2")[0].childNodes[0].nodeValue + "</p>" +
+                    "<p class=''>"+ x[i].getElementsByTagName("p3")[0].childNodes[0].nodeValue + "</p>" +
+            "</div>" +
+        "</div>");
     
 }
